63a56592d97b6a75108be1dff4459c7cdad81fb7
FedeFreire <bandoleir@gmail.com>
2023-11-08 08:50:55 -0500

    Task 6 done

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import { NavLink } from 'react-router-dom';
+import { CartContext } from '../contexts/CartContext';
 
-const Navigation = props => {
+
+function Navigation() {
+	const { cart } = useContext(CartContext);
 	return (
 		<div className="navigation">
 			<NavLink to="/">Products</NavLink>
 			<NavLink to="/cart">
-				Cart <span>{props.cart.length}</span>
+				Cart <span>{cart.length}</span>
 			</NavLink>
 		</div>
 	);
-};
+}
+
+
+// const Navigation = props => {
+// 	return (
+// 		<div className="navigation">
+// 			<NavLink to="/">Products</NavLink>
+// 			<NavLink to="/cart">
+// 				Cart <span>{props.cart.length}</span>
+// 			</NavLink>
+// 		</div>
+// 	);
+// };
 
 export default Navigation;
--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -1,18 +1,15 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import { CartContext } from '../contexts/CartContext';
+
 
 // Components
 import Item from './ShoppingCartItem';
 
-const ShoppingCart = props => {
-	const getCartTotal = () => {
-		return props.cart.reduce((acc, value) => {
-			return acc + value.price;
-		}, 0).toFixed(2);
-	};
-
+function ShoppingCart() {
+	const { cart } = useContext(CartContext);
 	return (
 		<div className="shopping-cart">
-			{props.cart.map(item => (
+			{cart.map(item => (
 				<Item key={item.id} {...item} />
 			))}
 
@@ -22,6 +19,29 @@ const ShoppingCart = props => {
 			</div>
 		</div>
 	);
-};
+}
+
+
+
+// const ShoppingCart = props => {
+// 	const getCartTotal = () => {
+// 		return props.cart.reduce((acc, value) => {
+// 			return acc + value.price;
+// 		}, 0).toFixed(2);
+// 	};
+
+// 	return (
+// 		<div className="shopping-cart">
+// 			{props.cart.map(item => (
+// 				<Item key={item.id} {...item} />
+// 			))}
+
+// 			<div className="shopping-cart__checkout">
+// 				<p>Total: ${getCartTotal()}</p>
+// 				<button>Checkout</button>
+// 			</div>
+// 		</div>
+// 	);
+// };
 
 export default ShoppingCart;
